ca936543d2f9231058620a5d2f14639aa9e6a009
Rakinzi Silver <93699315+Rakinzi@users.noreply.github.com>
2024-04-19 20:11:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -128,5 +128,5 @@ def search_for_images():
             return jsonify(images)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0")
